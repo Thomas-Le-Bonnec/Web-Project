e1a7b7a60f6a8925f6edcb3fc12d3044bf5a68cc
TheBonnec <thomaslebonnec@icloud.com>
2023-12-02 23:47:41 -0500

    first commit

---
+++ data/data.js
@@ -0,0 +1,27 @@
+const cities = [
+    {
+        title: "Toronto",
+        desc: "Toronto is the capital city of the Canadian province of Ontario. It is the most populous city in Canada and the fourth most populous city in North America. Known for its diverse cultural scene, Toronto is a major financial and business hub.",
+        img: "https://cdn.britannica.com/93/94493-050-35524FED/Toronto.jpg",
+    },
+    {
+        title: "Montréal",
+        desc: "Montréal is the largest city in the province of Quebec and the second-largest city in Canada. It is known for its rich history, diverse culture, and vibrant arts scene. Montréal is a bilingual city with a mix of French and English influences.",
+        img: "https://onedayonetravel.com/wp-content/uploads/2019/03/Où-voyage-en-2019-Montréal-neige.jpg",
+    },
+    {
+        title: "Québec",
+        desc: "Québec City is the capital of the province of Quebec. It is one of the oldest cities in North America, known for its historic architecture and European charm. The city is situated on the banks of the Saint Lawrence River.",
+        img: "https://media.routard.com/image/28/9/fb-quebec.1548289.jpg",
+    },
+    {
+        title: "Ottawa",
+        desc: "Ottawa is the capital city of Canada. Located on the Ottawa River, it is known for its impressive government buildings, historic landmarks, and cultural institutions. Ottawa is a bilingual city with a mix of English and French-speaking residents.",
+        img: "https://d1l57x9nwbbkz.cloudfront.net/files/s3fs-public/2023-07/parliament-buildings-ottawa.jpg?VersionId=lTIbTpBZ7OZHw4RzFgcNTEQ_17bN11nL",
+    },
+    {
+        title: "Vancouver",
+        desc: "Vancouver is a coastal city in British Columbia, known for its stunning natural scenery and diverse population. Surrounded by mountains and the Pacific Ocean, Vancouver is a major center for film production and a popular destination for outdoor activities.",
+        img: "https://res.cloudinary.com/simpleview/image/upload/v1589990523/clients/vancouverbc/Vancouver_Aerial_2017_1__72115131-4a31-42dc-b369-7a5ccec8273f.jpg",
+    },
+];
